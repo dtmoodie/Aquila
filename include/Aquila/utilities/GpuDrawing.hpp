2aba1abc56172c75ced9da02293dad9a1ff2af77
dan <dtmoodie@gmail.com>
2017-03-12 20:21:45 -0400

    Missed windows export.

--- include/Aquila/utilities/GpuDrawing.hpp
+++ include/Aquila/utilities/GpuDrawing.hpp
@@ -1,10 +1,10 @@
 #pragma once
 #include <opencv2/core.hpp>
-
+#include <Aquila/Detail/Export.hpp>
 namespace cv
 {
 namespace cuda
 {
-    void rectangle(cv::cuda::GpuMat& img, const cv::Rect& rect, cv::Scalar color, int thickness, cv::cuda::Stream& stream);
+	AQUILA_EXPORTS void rectangle(cv::cuda::GpuMat& img, const cv::Rect& rect, cv::Scalar color, int thickness, cv::cuda::Stream& stream);
 }
 }
