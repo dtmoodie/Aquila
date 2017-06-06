17a46252656dd41779c0e47746329dd280c37b12
dan <dtmoodie@gmail.com>
2017-06-06 17:12:20 -0400

    minor bug fixes. update syncedmemory input param to reflect bug fixes in metaobject. ogl allocators should not set system table singleton in constructor.

--- modules/rcc/src/Aquila/rcc/SystemTable.hpp
+++ modules/rcc/src/Aquila/rcc/SystemTable.hpp
@@ -1,62 +1,66 @@
 #pragma once
 #include <Aquila/core/detail/Export.hpp>
-#include <RuntimeObjectSystem/shared_ptr.hpp>
 #include <MetaObject/detail/TypeInfo.hpp>
+#include <RuntimeObjectSystem/shared_ptr.hpp>
 #include <map>
 #include <memory>
-struct AQUILA_EXPORTS ISingleton
-{
-    virtual ~ISingleton(){}
+struct AQUILA_EXPORTS ISingleton {
+    virtual ~ISingleton() {}
 };
 
-template<typename T> struct Singleton: public ISingleton
-{
-    Singleton(T* ptr_): ptr(ptr_){}
-    virtual ~Singleton()
-    {
+template <typename T>
+struct Singleton : public ISingleton {
+    Singleton(T* ptr_)
+        : ptr(ptr_) {}
+    virtual ~Singleton() {
         delete ptr;
     }
     T* ptr;
-    operator T*() const {return ptr;}
+    operator T*() const { return ptr; }
 };
 
-template<typename T> struct IObjectSingleton: public ISingleton
-{
+template <typename T>
+struct IObjectSingleton : public ISingleton {
     rcc::shared_ptr<T> ptr;
-    IObjectSingleton(T* ptr_): ptr(ptr_){}
-    IObjectSingleton(const rcc::shared_ptr<T>& ptr_): ptr(ptr_){}
-    operator T*() const {return ptr.get(); }
+    IObjectSingleton(T* ptr_)
+        : ptr(ptr_) {}
+    IObjectSingleton(const rcc::shared_ptr<T>& ptr_)
+        : ptr(ptr_) {}
+    operator T*() const { return ptr.get(); }
 };
 
-struct AQUILA_EXPORTS SystemTable
-{
+struct AQUILA_EXPORTS SystemTable {
     SystemTable();
     void cleanUp();
     // These are per stream singletons
-    template<typename T> T* getSingleton()
-    {
+    template <typename T>
+    T* getSingleton() {
         auto g_itr = g_singletons.find(mo::TypeInfo(typeid(T)));
-        if(g_itr != g_singletons.end())
-        {
+        if (g_itr != g_singletons.end()) {
             return static_cast<Singleton<T>*>(g_itr->second.get())->ptr;
         }
         return nullptr;
     }
 
-    template<typename T>
-    T* setSingleton(T* singleton, int stream_id = -1)
-    {
+    template <typename T>
+    typename std::enable_if<!std::is_base_of<IObject, T>::value, T*>::type setSingleton(T* singleton) {
         g_singletons[mo::TypeInfo(typeid(T))] = std::shared_ptr<ISingleton>(new Singleton<T>(singleton));
+        return singleton;
+    }
+
+    template <typename T>
+    typename std::enable_if<std::is_base_of<IObject, T>::value, T*>::type setSingleton(T* singleton) {
+        g_singletons[mo::TypeInfo(typeid(T))] = std::shared_ptr<ISingleton>(new IObjectSingleton<T>(singleton));
         return singleton;
     }
 
     void deleteSingleton(mo::TypeInfo type);
 
-    template<typename T>
-    void deleteSingleton()
-    {
+    template <typename T>
+    void deleteSingleton() {
         deleteSingleton(mo::TypeInfo(typeid(T)));
     }
+
 private:
-    std::map<mo::TypeInfo, std::shared_ptr<ISingleton>> g_singletons;
+    std::map<mo::TypeInfo, std::shared_ptr<ISingleton> > g_singletons;
 };
--- modules/utilities/src/Aquila/utilities/cuda/ogl_allocators.cpp
+++ modules/utilities/src/Aquila/utilities/cuda/ogl_allocators.cpp
@@ -1,6 +1,6 @@
 #include "Aquila/utilities/cuda/ogl_allocators.h"
-#include "RuntimeObjectSystem/ObjectInterfacePerModule.h"
 #include "Aquila/rcc/SystemTable.hpp"
+#include "RuntimeObjectSystem/ObjectInterfacePerModule.h"
 #include <Aquila/rcc/external_includes/cv_core.hpp>
 #ifdef HAVE_QT
 #include <QOpenGLContext>
@@ -8,21 +8,17 @@
 #endif
 #include <RuntimeObjectSystem/ISimpleSerializer.h>
 
-
 using namespace aq;
 
-void ogl_allocator::nodeInit(bool firstInit)
-{
-    if(!firstInit)
-    {
-        if(is_default_allocator)
+void ogl_allocator::nodeInit(bool firstInit) {
+    if (!firstInit) {
+        if (is_default_allocator)
             cv::cuda::GpuMat::setDefaultAllocator(this);
     }
 }
-void ogl_allocator::Serialize(ISimpleSerializer* pSerializer)
-{
+void ogl_allocator::Serialize(ISimpleSerializer* pSerializer) {
     IObject::Serialize(pSerializer);
-    if(cv::cuda::GpuMat::defaultAllocator() == this)
+    if (cv::cuda::GpuMat::defaultAllocator() == this)
         is_default_allocator = true;
     SERIALIZE(_pool);
     SERIALIZE(mapped_buffers);
@@ -40,35 +36,23 @@ void ogl_allocator::Serialize(ISimpleSerializer* pSerializer)
     SERIALIZE(ogl_allocator::memoryUsage);
 }
 //size_t initial_pool_size = 10000000, size_t threshold_level = 1000000
-ogl_allocator::ogl_allocator():
-    CombinedAllocator(10000000, 1000000), is_default_allocator(false)
-{
-    auto table = PerModuleInterface::GetInstance()->GetSystemTable();
-    if(table)
-        table->setSingleton<ogl_allocator>(this);
+ogl_allocator::ogl_allocator()
+    : CombinedAllocator(10000000, 1000000)
+    , is_default_allocator(false) {
 }
-bool ogl_allocator::allocate(cv::cuda::GpuMat* mat, int rows, int cols, size_t elemSize)
-{
+bool ogl_allocator::allocate(cv::cuda::GpuMat* mat, int rows, int cols, size_t elemSize) {
 
-    auto id = boost::this_thread::get_id();
+    auto id         = boost::this_thread::get_id();
     auto scope_name = currentScopeName[id];
-    auto scope = std::make_tuple(scope_name, rows, cols, mat->type(), id, false);
-    auto itr = std::find_if(buffer_request_locations.begin(), buffer_request_locations.end(), [&scope](std::tuple<std::string, int, int, int, boost::thread::id, bool>& other)->bool
-    {
-        return std::get<0>(other) == std::get<0>(scope) &&
-               std::get<1>(other) == std::get<1>(scope) &&
-               std::get<2>(other) == std::get<2>(scope) &&
-               std::get<3>(other) == std::get<3>(scope) &&
-               std::get<4>(other) == std::get<4>(scope);
+    auto scope      = std::make_tuple(scope_name, rows, cols, mat->type(), id, false);
+    auto itr        = std::find_if(buffer_request_locations.begin(), buffer_request_locations.end(), [&scope](std::tuple<std::string, int, int, int, boost::thread::id, bool>& other) -> bool {
+        return std::get<0>(other) == std::get<0>(scope) && std::get<1>(other) == std::get<1>(scope) && std::get<2>(other) == std::get<2>(scope) && std::get<3>(other) == std::get<3>(scope) && std::get<4>(other) == std::get<4>(scope);
     });
 
-    if(itr == buffer_request_locations.end())
-    {
+    if (itr == buffer_request_locations.end()) {
         buffer_request_locations.push_back(scope);
-    }else
-    {
-        if(std::get<5>(*itr))
-        {
+    } else {
+        if (std::get<5>(*itr)) {
             // Allocate from opengl buffer
             {
 
@@ -87,12 +71,10 @@ bool ogl_allocator::allocate(cv::cuda::GpuMat* mat, int rows, int cols, size_t e
                 }*/
 
                 std::lock_guard<std::mutex> pool_lock(aq::pool::ObjectPool<cv::ogl::Buffer>::mtx);
-                for(auto itr = _pool.begin(); itr != _pool.end(); ++itr)
-                {
-                    if((*itr)->rows() == rows && (*itr)->cols() == cols && (*itr)->type() == mat->type())
-                    {
-                        *mat = (*itr)->mapDevice();
-                        mat->allocator = this;
+                for (auto itr = _pool.begin(); itr != _pool.end(); ++itr) {
+                    if ((*itr)->rows() == rows && (*itr)->cols() == cols && (*itr)->type() == mat->type()) {
+                        *mat                      = (*itr)->mapDevice();
+                        mat->allocator            = this;
                         mapped_buffers[mat->data] = *itr;
                         _pool.erase(itr);
                         return true;
@@ -100,63 +82,53 @@ bool ogl_allocator::allocate(cv::cuda::GpuMat* mat, int rows, int cols, size_t e
                 }
             }
             // Unable to allocate from one of the currently unused opengl buffers, so instead we will create a new buffer
-            auto buf = new cv::ogl::Buffer(rows, cols, mat->type(), cv::ogl::Buffer::PIXEL_UNPACK_BUFFER);
-            *mat = buf->mapDevice();
+            auto buf                  = new cv::ogl::Buffer(rows, cols, mat->type(), cv::ogl::Buffer::PIXEL_UNPACK_BUFFER);
+            *mat                      = buf->mapDevice();
             mapped_buffers[mat->data] = buf;
-            mat->allocator = this;
+            mat->allocator            = this;
             return true;
         }
     }
     return CombinedAllocator::allocate(mat, rows, cols, elemSize);
 }
 
-void ogl_allocator::free(cv::cuda::GpuMat* mat)
-{
+void ogl_allocator::free(cv::cuda::GpuMat* mat) {
     CombinedAllocator::free(mat);
 }
 
-
 // Creates a GpuMat from an opengl buffer
-cv::cuda::GpuMat ogl_allocator::createMat(int rows, int cols, int type, cv::cuda::Stream& stream)
-{
+cv::cuda::GpuMat ogl_allocator::createMat(int rows, int cols, int type, cv::cuda::Stream& stream) {
 
     {
         std::lock_guard<std::mutex> pool_lock(aq::pool::ObjectPool<cv::ogl::Buffer>::mtx);
-        for (auto itr = _pool.begin(); itr != _pool.end(); ++itr)
-        {
-            if ((*itr)->rows() == rows && (*itr)->cols() == cols && (*itr)->type() == type)
-            {
-                auto mat = (*itr)->mapDevice(stream);
-                mat.allocator = this;
+        for (auto itr = _pool.begin(); itr != _pool.end(); ++itr) {
+            if ((*itr)->rows() == rows && (*itr)->cols() == cols && (*itr)->type() == type) {
+                auto mat                 = (*itr)->mapDevice(stream);
+                mat.allocator            = this;
                 mapped_buffers[mat.data] = *itr;
                 _pool.erase(itr);
                 return mat;
             }
         }
     }
-    cv::ogl::Buffer* buffer = new cv::ogl::Buffer(rows, cols, type, cv::ogl::Buffer::PIXEL_UNPACK_BUFFER);
-    auto mat = buffer->mapDevice(stream);
+    cv::ogl::Buffer* buffer  = new cv::ogl::Buffer(rows, cols, type, cv::ogl::Buffer::PIXEL_UNPACK_BUFFER);
+    auto             mat     = buffer->mapDevice(stream);
     mapped_buffers[mat.data] = buffer;
     return mat;
 }
 
-aq::pool::Ptr<cv::ogl::Buffer> ogl_allocator::get_ogl_buffer(const cv::cuda::GpuMat& mat, cv::cuda::Stream& stream)
-{
+aq::pool::Ptr<cv::ogl::Buffer> ogl_allocator::get_ogl_buffer(const cv::cuda::GpuMat& mat, cv::cuda::Stream& stream) {
 
     auto itr = mapped_buffers.find(mat.data);
-    if(itr != mapped_buffers.end())
-    {
+    if (itr != mapped_buffers.end()) {
         itr->second->unmapDevice(stream);
         return pool::Ptr<cv::ogl::Buffer>(itr->second, this);
-    }else
-    {
+    } else {
         // Matrix wasn't originally mapped from an opengl buffer, thus we need to update buffer_request_locations to indicate that those gpumats should be allocated from opengl memory
         // List it as a GpuMat that needs to be created from an opengl buffer at next chance
         auto scope_name = scopeOwnership.find(mat.data);
-        if (scope_name != scopeOwnership.end())
-        {
-            auto scope_itr = std::find_if(buffer_request_locations.begin(), buffer_request_locations.end(), [&](std::tuple<std::string, int, int, int, boost::thread::id, bool>& other)->bool
-            {
+        if (scope_name != scopeOwnership.end()) {
+            auto scope_itr = std::find_if(buffer_request_locations.begin(), buffer_request_locations.end(), [&](std::tuple<std::string, int, int, int, boost::thread::id, bool>& other) -> bool {
                 return std::get<0>(other) == scope_name->second && std::get<1>(other) == mat.rows && std::get<2>(other) == mat.cols && std::get<3>(other) == mat.type();
             });
             std::get<5>(*scope_itr) = true;
@@ -164,10 +136,8 @@ aq::pool::Ptr<cv::ogl::Buffer> ogl_allocator::get_ogl_buffer(const cv::cuda::Gpu
         // Find any unused opengl buffers of the correct size
         {
             std::lock_guard<std::mutex> pool_lock(aq::pool::ObjectPool<cv::ogl::Buffer>::mtx);
-            for(auto buf: _pool)
-            {
-                if(buf->rows() == mat.rows && buf->cols() == mat.cols && buf->type() == mat.type())
-                {
+            for (auto buf : _pool) {
+                if (buf->rows() == mat.rows && buf->cols() == mat.cols && buf->type() == mat.type()) {
                     buf->copyFrom(mat, stream, cv::ogl::Buffer::PIXEL_UNPACK_BUFFER);
                     return pool::Ptr<cv::ogl::Buffer>(buf, this);
                 }
@@ -180,16 +150,13 @@ aq::pool::Ptr<cv::ogl::Buffer> ogl_allocator::get_ogl_buffer(const cv::cuda::Gpu
         return pool::Ptr<cv::ogl::Buffer>(buf, this);
     }
 }
-aq::pool::Ptr<cv::ogl::Buffer> ogl_allocator::get_ogl_buffer(const cv::Mat& mat)
-{
+aq::pool::Ptr<cv::ogl::Buffer> ogl_allocator::get_ogl_buffer(const cv::Mat& mat) {
 
     {
         std::lock_guard<std::mutex> pool_lock(aq::pool::ObjectPool<cv::ogl::Buffer>::mtx);
         // Find any unused opengl buffers of the correct size
-        for (auto buf : _pool)
-        {
-            if (buf->rows() == mat.rows && buf->cols() == mat.cols && buf->type() == mat.type())
-            {
+        for (auto buf : _pool) {
+            if (buf->rows() == mat.rows && buf->cols() == mat.cols && buf->type() == mat.type()) {
                 buf->copyFrom(mat, cv::ogl::Buffer::PIXEL_UNPACK_BUFFER);
                 return pool::Ptr<cv::ogl::Buffer>(buf, this);
             }
@@ -202,4 +169,4 @@ aq::pool::Ptr<cv::ogl::Buffer> ogl_allocator::get_ogl_buffer(const cv::Mat& mat)
     //used_ogl_buffers.push_back(buf);
     return pool::Ptr<cv::ogl::Buffer>(buf, this);
 }
-REGISTERSINGLETON(ogl_allocator, true);
+//REGISTERSINGLETON(ogl_allocator, true);
