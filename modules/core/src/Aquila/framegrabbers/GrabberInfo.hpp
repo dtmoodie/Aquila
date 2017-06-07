59f0596eaa2a2133bb4095b4e466c7ec2b91df0c
dan <dtmoodie@gmail.com>
2017-06-06 21:30:48 -0400

    case corrections.

--- modules/core/src/Aquila/framegrabbers/GrabberInfo.hpp
+++ modules/core/src/Aquila/framegrabbers/GrabberInfo.hpp
@@ -3,29 +3,29 @@
 #include "IFrameGrabber.hpp"
 namespace Grabbers
 {
-    DEFINE_HAS_STATIC_FUNCTION(HasCanLoad, CanLoad, int(*)(const std::string&));
+    DEFINE_HAS_STATIC_FUNCTION(HasCanLoad, canLoad, int(*)(const std::string&));
     template<class U> int HasLoadHelper(const std::string& path, typename std::enable_if<HasCanLoad<U>::value, void>::type* = 0)
     {
-        return U::CanLoad(path);
+        return U::canLoad(path);
     }
     template<class U> int HasLoadHelper(const std::string& path, typename std::enable_if<!HasCanLoad<U>::value, void>::type* = 0)
     {
         (void)path;
         return 0;
     }
-    DEFINE_HAS_STATIC_FUNCTION(HasListPaths, ListPaths, void(*)(std::vector<std::string>&));
+    DEFINE_HAS_STATIC_FUNCTION(HasListPaths, listPaths, void(*)(std::vector<std::string>&));
     template<class U> void HasListHelper(std::vector<std::string>& paths, typename std::enable_if<HasListPaths<U>::value, void>::type* = 0)
     {
-        U::ListPaths(paths);
+        U::listPaths(paths);
     }
     template<class U> void HasListHelper(std::vector<std::string>& path, typename std::enable_if<!HasListPaths<U>::value, void>::type* = 0)
     {
         (void)path;
     }
-    DEFINE_HAS_STATIC_FUNCTION(HasTimeout, Timeout, int(*)(void));
+    DEFINE_HAS_STATIC_FUNCTION(HasTimeout, loadTimeout, int(*)(void));
     template<class U> int HasTimeoutHelper(typename std::enable_if<HasTimeout<U>::value, void>::type* = 0)
     {
-        return U::Timeout();
+        return U::loadTimeout();
     }
     template<class U> int HasTimeoutHelper(typename std::enable_if<!HasTimeout<U>::value, void>::type* = 0)
     {
@@ -39,15 +39,15 @@ namespace mo
     template<class Type>
     struct MetaObjectInfoImpl<Type, aq::Nodes::GrabberInfo> : public aq::Nodes::GrabberInfo
     {
-        int CanLoad(const std::string& path) const
+        int canLoad(const std::string& path) const
         {
             return Grabbers::HasLoadHelper<Type>(path);
         }
-        void ListPaths(std::vector<std::string>& paths) const
+        void listPaths(std::vector<std::string>& paths) const
         {
             Grabbers::HasListHelper<Type>(paths);
         }
-        int Timeout() const
+        int timeout() const
         {
             return Grabbers::HasTimeoutHelper<Type>();
         }
