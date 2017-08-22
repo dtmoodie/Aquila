8ece3ddf98471f5add8366ef09feb5d8eed5f160
dan <dtmoodie@gmail.com>
2017-08-22 09:06:04 -0400

    update

--- modules/gui/src/Aquila/plotting/Plotter.h
+++ modules/gui/src/Aquila/plotting/Plotter.h
@@ -2,7 +2,7 @@
 #include <Aquila/core/detail/Export.hpp>
 #include <MetaObject/object/IMetaObject.hpp>
 #include <MetaObject/object/detail/MetaObjectMacros.hpp>
-#include <MetaObject/Signals/detail/SlotMacros.hpp>
+#include <MetaObject/signals/detail/SlotMacros.hpp>
 #include <MetaObject/params/ParamMacros.hpp>
 #include <IObjectInfo.h>
 #include <list>
