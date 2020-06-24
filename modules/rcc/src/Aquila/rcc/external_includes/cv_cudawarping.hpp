561d4bde40fefce49ebeafa8b20b05188d8facb5
dan <dtmoodie@gmail.com>
2020-06-24 08:35:24 -0400

    pragma once -> include guards

--- modules/rcc/src/Aquila/rcc/external_includes/cv_cudawarping.hpp
+++ modules/rcc/src/Aquila/rcc/external_includes/cv_cudawarping.hpp
@@ -1,10 +1,11 @@
-#pragma once
+#ifndef RCC_EXTERNAL_CV_CUDAWARPING_HPP
+#define RCC_EXTERNAL_CV_CUDAWARPING_HPP
 #include <MetaObject/core/metaobject_config.hpp>
-#if MO_OPENCV_HAVE_CUDA
 
 #include "RuntimeObjectSystem/RuntimeLinkLibrary.h"
 #include "cv_link_config.hpp"
 #include "opencv2/cudawarping.hpp"
+
 #ifdef _MSC_VER // Windows
 
 #ifdef _DEBUG
@@ -14,7 +15,10 @@ RUNTIME_COMPILER_LINKLIBRARY("opencv_cudawarping" CV_VERSION_ ".lib")
 #endif
 
 #else // Linux
+
 RUNTIME_COMPILER_LINKLIBRARY("-lopencv_cudawarping")
 #define CALL
+
 #endif
-#endif
+
+#endif // RCC_EXTERNAL_CV_CUDAWARPING_HPP
