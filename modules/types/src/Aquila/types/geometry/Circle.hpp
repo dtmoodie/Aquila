4ad9b442cc8125d1433c12dceb41df244f3e81a3
dan <dtmoodie@gmail.com>
2020-08-03 08:28:39 -0400

    fix whitespace

--- modules/types/src/Aquila/types/geometry/Circle.hpp
+++ modules/types/src/Aquila/types/geometry/Circle.hpp
@@ -1,28 +1,42 @@
 #pragma once
 #include <Eigen/Core>
+
 #include <ct/reflect.hpp>
+#include <ct/types/eigen.hpp>
 
 namespace aq
 {
-template <class T>
-struct Circle
-{
+    template <class T>
+    struct Circle
+    {
 
-    Circle(T x = 0.0, T y = 0.0, T rad = 0) : origin(x, y), radius(rad) {}
+        Circle(T x = 0.0, T y = 0.0, T rad = 0)
+            : origin(x, y)
+            , radius(rad)
+        {
+        }
 
-    Circle(Eigen::Matrix<T, 2, 1> og, T rad = 0.0) : origin(og), radius(rad) {}
+        Circle(Eigen::Matrix<T, 2, 1> og, T rad = 0.0)
+            : origin(og)
+            , radius(rad)
+        {
+        }
 
-    bool operator==(const aq::Circle<float>& other) { return origin == other.origin && radius == other.radius; }
-    Eigen::Matrix<T, 2, 1> origin;
-    T radius;
-};
-}
+        bool operator==(const aq::Circle<float>& other)
+        {
+            return origin == other.origin && radius == other.radius;
+        }
+        Eigen::Matrix<T, 2, 1> origin;
+        T radius;
+    };
+    using Circlef = Circle<float>;
+
+} // namespace aq
 
 namespace ct
 {
-REFLECT_TEMPLATED_START(aq::Circle)
-    PUBLIC_ACCESS(radius)
-// PUBLIC_ACCESS(origin)
-REFLECT_END;
-
-}
+    REFLECT_TEMPLATED_BEGIN(aq::Circle)
+        PUBLIC_ACCESS(radius)
+        PUBLIC_ACCESS(origin)
+    REFLECT_END;
+} // namespace ct
