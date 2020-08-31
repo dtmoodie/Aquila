3994aebb629c2bddc271ea36b477bf3858be4978
asiansensation <dtmoodie@gmail.com>
2020-08-31 18:34:48 +0000

    formatting

--- modules/core/src/Aquila/framegrabbers/IFrameGrabber.hpp
+++ modules/core/src/Aquila/framegrabbers/IFrameGrabber.hpp
@@ -26,92 +26,93 @@ RUNTIME_COMPILER_SOURCEDEPENDENCY
 
 namespace aq
 {
-namespace nodes
-{
-class IFrameGrabber;
-class FrameGrabberInfo;
-class Grabber;
-class GrabberInfo;
-}
-}
+    namespace nodes
+    {
+        class IFrameGrabber;
+        class FrameGrabberInfo;
+        class Grabber;
+        class GrabberInfo;
+    } // namespace nodes
+} // namespace aq
 
 namespace aq
 {
-class IGraph;
+    class IGraph;
 
-namespace nodes
-{
+    namespace nodes
+    {
 
-class AQUILA_EXPORTS GrabberInfo : virtual public mo::IMetaObjectInfo
-{
-  public:
-    virtual int canLoad(const std::string& path) const;
-    virtual void listPaths(std::vector<std::string>& paths) const;
-    virtual int timeout() const;
-};
+        class AQUILA_EXPORTS GrabberInfo : virtual public mo::IMetaObjectInfo
+        {
+          public:
+            virtual int canLoad(const std::string& path) const;
+            virtual void listPaths(std::vector<std::string>& paths) const;
+            virtual int timeout() const;
+        };
 
-class AQUILA_EXPORTS IGrabber : virtual public TInterface<IGrabber, Algorithm>
-{
-  public:
-    typedef GrabberInfo InterfaceInfo;
-    typedef IGrabber Interface;
-    typedef rcc::shared_ptr<IGrabber> Ptr;
-    MO_DERIVE(IGrabber, Algorithm)
-        PARAM(std::string, loaded_document, "")
-    MO_END;
+        class AQUILA_EXPORTS IGrabber : virtual public TInterface<IGrabber, Algorithm>
+        {
+          public:
+            typedef GrabberInfo InterfaceInfo;
+            typedef IGrabber Interface;
+            typedef rcc::shared_ptr<IGrabber> Ptr;
+            MO_DERIVE(IGrabber, Algorithm)
+                PARAM(std::string, loaded_document, "")
+            MO_END;
 
-    virtual bool loadData(const std::string& path) = 0;
-    virtual bool grab() = 0;
+            virtual bool loadData(const std::string& path) = 0;
+            virtual bool grab() = 0;
 
-  protected:
-    virtual bool processImpl() override;
-};
+          protected:
+            virtual bool processImpl() override;
+        };
 
-class AQUILA_EXPORTS FrameGrabberInfo : virtual public NodeInfo
-{
-  public:
-    /*!
-     * \brief canLoadPath determines if the frame grabber associated with this info object can load an input
-     * document
-     * \param document is a string descibing a file / path / URI to load
-     * \return 0 if the document cannot be loaded, priority of the frame grabber otherwise.  Higher value means
-     * higher compatibility with this document
-     */
-    virtual int canLoadPath(const std::string& document) const;
-    /*!
-     * \brief loadTimeout returns the ms that should be allowed for the frame grabber's LoadFile function before
-     * a timeout condition
-     * \return timeout in ms
-     */
-    virtual int loadTimeout() const;
+        class AQUILA_EXPORTS FrameGrabberInfo : virtual public NodeInfo
+        {
+          public:
+            /*!
+             * \brief canLoadPath determines if the frame grabber associated with this info object can load an input
+             * document
+             * \param document is a string descibing a file / path / URI to load
+             * \return 0 if the document cannot be loaded, priority of the frame grabber otherwise.  Higher value means
+             * higher compatibility with this document
+             */
+            virtual int canLoadPath(const std::string& document) const;
+            /*!
+             * \brief loadTimeout returns the ms that should be allowed for the frame grabber's LoadFile function before
+             * a timeout condition
+             * \return timeout in ms
+             */
+            virtual int loadTimeout() const;
 
-    // Function used for listing what documents are available for loading, used in cases of connected devices to
-    // list what
-    // devices have been enumerated
-    virtual std::vector<std::string> listLoadablePaths() const;
+            // Function used for listing what documents are available for loading, used in cases of connected devices to
+            // list what
+            // devices have been enumerated
+            virtual std::vector<std::string> listLoadablePaths() const;
 
-    std::string Print(IObjectInfo::Verbosity verbosity = IObjectInfo::INFO) const;
-};
+            std::string Print(IObjectInfo::Verbosity verbosity = IObjectInfo::INFO) const;
+        };
 
-// Interface class for the base level of features frame grabber
-class AQUILA_EXPORTS IFrameGrabber : virtual public TInterface<IFrameGrabber, Node>
-{
-  public:
-    typedef FrameGrabberInfo InterfaceInfo;
-    typedef IFrameGrabber Interface;
+        // Interface class for the base level of features frame grabber
+        class AQUILA_EXPORTS IFrameGrabber : virtual public TInterface<IFrameGrabber, Node>
+        {
+          public:
+            typedef FrameGrabberInfo InterfaceInfo;
+            typedef IFrameGrabber Interface;
 
-    static rcc::shared_ptr<IFrameGrabber> create(const std::string& doc, const std::string& preferred_loader = "");
-    // Returns all data sources that can be loaded with the name of the loader that can load it
-    static std::vector<std::pair<std::string, std::string>> listAllLoadableDocuments();
+            static rcc::shared_ptr<IFrameGrabber> create(const std::string& doc,
+                                                         const std::string& preferred_loader = "");
+            // Returns all data sources that can be loaded with the name of the loader that can load it
+            static std::vector<std::pair<std::string, std::string>> listAllLoadableDocuments();
 
-    MO_DERIVE(IFrameGrabber, Node)
-        MO_SIGNAL(void, update)
-        MO_SLOT(void, restart)
-        PARAM(std::vector<std::string>, loaded_document, {})
-        PARAM_UPDATE_SLOT(loaded_document)
-        MO_SLOT(bool, loadData, std::string)
-        MO_SLOT(bool, loadData, std::vector<std::string>)
-    MO_END;
-};
-} // namespace nodes
+            MO_DERIVE(IFrameGrabber, Node)
+                MO_SIGNAL(void, update)
+                MO_SLOT(void, restart)
+                PARAM(std::vector<std::string>, loaded_document, {})
+                PARAM_UPDATE_SLOT(loaded_document)
+                MO_SLOT(bool, loadData, std::string)
+                MO_SLOT(bool, loadData, std::vector<std::string>)
+            MO_END;
+        };
+    } // namespace nodes
 } // namespace aq
