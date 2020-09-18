6aae61b958db8f7894e2f008db312ab69659a723
dan <dtmoodie@gmail.com>
2020-09-18 13:55:08 -0400

    fix namespace and naming of aq::detection::Path

--- modules/types/src/Aquila/types/DetectionPatch.hpp
+++ modules/types/src/Aquila/types/DetectionPatch.hpp
@@ -4,27 +4,31 @@
 
 namespace aq
 {
-    struct DetectionPatch
+    namespace detection
     {
-        aq::SyncedImage source_image;
-        aq::SyncedImage patch;
-    };
+        struct Patch
+        {
+            aq::SyncedImage source_image;
+            aq::SyncedImage patch;
+        };
 
-    struct AlignedPatch
-    {
-        aq::SyncedImage source_image;
-        aq::SyncedImage aligned_patch;
-    };
+        struct AlignedPatch
+        {
+            aq::SyncedImage source_image;
+            aq::SyncedImage aligned_patch;
+        };
+    } // namespace detection
+
 } // namespace aq
 
 namespace ct
 {
-    REFLECT_BEGIN(aq::DetectionPatch)
+    REFLECT_BEGIN(aq::detection::Patch)
         PUBLIC_ACCESS(source_image)
         PUBLIC_ACCESS(patch)
     REFLECT_END;
 
-    REFLECT_BEGIN(aq::AlignedPatch)
+    REFLECT_BEGIN(aq::detection::AlignedPatch)
         PUBLIC_ACCESS(source_image)
         PUBLIC_ACCESS(aligned_patch)
     REFLECT_END;
