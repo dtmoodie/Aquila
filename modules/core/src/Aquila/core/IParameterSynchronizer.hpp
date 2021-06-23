a5c9ee5b7521ea248ce89ac5b9b16b619cbda60a
dan <dtmoodie@gmail.com>
2021-06-22 21:35:42 -0400

    add initial ParameterSynchronizer files

---
+++ modules/core/src/Aquila/core/IParameterSynchronizer.hpp
@@ -0,0 +1,13 @@
+#ifndef AQ_CORE_IPARAMETER_SYNCHRONIZER_HPP
+#define AQ_CORE_IPARAMETER_SYNCHRONIZER_HPP
+#include <Aquila/core/export.hpp>
+
+namespace aq
+{
+    class IParameterSynchronizer
+    {
+
+    };
+}
+
+#endif // AQ_CORE_IPARAMETER_SYNCHRONIZER_HPP
---
+++ modules/core/src/Aquila/core/ParameterSynchronizer.hpp
@@ -0,0 +1,10 @@
+#ifndef AQ_CORE_PARAMETER_SYNCHRONIZER_HPP
+#define AQ_CORE_PARAMETER_SYNCHRONIZER_HPP
+#include "IParameterSynchronizer.hpp"
+
+namespace aq
+{
+
+}
+
+#endif // AQ_CORE_PARAMETER_SYNCHRONIZER_HPP
